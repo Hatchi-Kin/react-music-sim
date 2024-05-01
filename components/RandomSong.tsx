afed7b5fd4ddee1e5c94b19800677ba8eac381bb
Hatchi-Kin <j.needham@hotmail.fr>
2024-05-01 23:44:34 +0200

    random song now displays cover art is available

--- components/RandomSong.tsx
+++ components/RandomSong.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 import { Card } from "@/components/ui/card";
 import { GiPerspectiveDiceSixFacesRandom } from "react-icons/gi";
 import AddToPlayListButton from "@/components/AddToPlayList";
+import Image from "next/image";
 
-// Define the shape of a song object
 export interface Song {
   row: {
     artist: string;
@@ -14,21 +14,21 @@ export interface Song {
     filepath: string;
   };
   lyrics: string;
+  artwork: string; // Base64 encoded image
 }
 
 const RandomSongCard = () => {
-  // State variables for the current song, loading status, and any error message
   const [song, setSong] = useState<Song | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const hasFetched = useRef(false); // Ref to track if data has been fetched
 
-  // Function to fetch a random song from the API
   const fetchRandomSong = useCallback(() => {
     setIsLoading(true);
     const token = localStorage.getItem("authToken") ?? "";
     const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-    fetch(`${baseUrl}/lyrics/random-lyrics`, {
+    fetch(`${baseUrl}/lyrics/random-lyrics-metadata`, {
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -52,12 +52,13 @@ const RandomSongCard = () => {
       });
   }, []);
 
-  // Fetch a random song when the component mounts
   useEffect(() => {
-    fetchRandomSong();
+    if (!hasFetched.current) {
+      fetchRandomSong();
+      hasFetched.current = true; // Mark as fetched
+    }
   }, [fetchRandomSong]);
 
-  // Show loading or error message if necessary
   if (isLoading) return <div className="text-white">Loading...</div>;
   if (error) return <div className="text-white">Error: {error}</div>;
   if (!song) return <div className="text-white">No song found</div>;
@@ -68,49 +69,65 @@ const RandomSongCard = () => {
         <button
           onClick={fetchRandomSong}
           className="
-            mt-5 mb-4 p-4 
-            bg-sky-600 hover:bg-sky-800 text-slate-300 
-            font-bold text-lg 
-            rounded 
-            flex justify-center items-center space-x-2"
+          mt-5 mb-4 p-4 
+          bg-sky-600 hover:bg-sky-800 text-slate-300 
+          font-bold text-lg 
+          rounded 
+          flex justify-center items-center space-x-2"
         >
           <GiPerspectiveDiceSixFacesRandom className="mr-3" size={24} />
           Refresh Song
           <GiPerspectiveDiceSixFacesRandom className="ml-3" size={24} />
         </button>
         {/* Display the current song */}
-        <div className="mt-4 p-8 w-full max-w-2xl">
-          <Card className="bg-[#141b2c] rounded-lg border-gray-700  text-slate-300 shadow-lg p-14 w-full">
-            <h3 className="text-3xl font-bold p-2">
-              {song.row.artist} - {song.row.title}
-            </h3>
-            <p className="text-xl p-2 mt-2">
-              Album: {song.row.album}, Track: {song.row.tracknumber}, Year: {song.row.year}
-            </p>
+        <div className="flex items-stretch gap-4 mt-4 mb-8 p-4 w-full max-w-4xl">
+          <Card
+            className={`bg-[#141b2c] rounded-lg border-gray-700 text-slate-300 shadow-lg p-6 ${song.artwork ? "grid grid-cols-3 gap-4" : ""} items-center h-64 w-full`}
+          >
+            <div
+              className={`col-span-2 ${song.artwork ? "" : "col-span-3"} flex flex-col justify-between overflow-auto`}
+            >
+              <h3 className="text-2xl font-bold p-2 break-words">{song.row.artist}</h3>
+              <h2 className="text-2xl font-bold p-2 break-words">{song.row.title}</h2>
+              <p className="text-xl p-2 mt-2 break-words">
+                Album: {song.row.album}, Track: {song.row.tracknumber}, Year: {song.row.year}
+              </p>
+            </div>
+            {song.artwork && (
+              <div className="relative h-full col-span-1">
+                <Image
+                  src={`data:image/jpeg;base64,${song.artwork}`}
+                  alt="Album Artwork"
+                  layout="fill"
+                  objectFit="contain"
+                  className="rounded-lg"
+                />
+              </div>
+            )}
           </Card>
         </div>
         <AddToPlayListButton song_full_path={song.row.filepath} size="large" />
+        {/* If the song has lyrics, display them */}
+        {song.lyrics && song.lyrics !== "No lyrics found for this song." && (
+          <div className="mt-4 p-8 w-full max-w-3xl mx-auto">
+            <Card className="fixed-height-card bg-[#141b2c] rounded-lg border-gray-700 text-slate-300 shadow-lg p-14 w-full">
+              {song.lyrics.split("\r\n", 1)[0] && (
+                <h3 className="text-2xl font-bold p-2">{song.lyrics.split("\r\n", 1)[0]}</h3>
+              )}
+              {song.lyrics
+                .split("\r\n")
+                .slice(1)
+                .join("\r\n")
+                .split("\n")
+                .map((line, index) => (
+                  <p key={index} className="text-xl text-slate-300 p-2 mt-1">
+                    {line}
+                  </p>
+                ))}
+            </Card>
+          </div>
+        )}
       </div>
-      {/* If the song has lyrics, display them */}
-      {song.lyrics && song.lyrics !== "No lyrics found for this song." && (
-        <div className="mt-4 p-8 w-full max-w-3xl mx-auto">
-          <Card className="bg-[#141b2c] rounded-lg border-gray-700 text-slate-300 shadow-lg p-14 w-full">
-            {song.lyrics.split("\r\n", 1)[0] && (
-              <h3 className="text-2xl font-bold p-2">{song.lyrics.split("\r\n", 1)[0]}</h3>
-            )}
-            {song.lyrics
-              .split("\r\n")
-              .slice(1)
-              .join("\r\n")
-              .split("\n")
-              .map((line, index) => (
-                <p key={index} className="text-xl text-slate-300 p-2 mt-1">
-                  {line}
-                </p>
-              ))}
-          </Card>
-        </div>
-      )}
     </div>
   );
 };
