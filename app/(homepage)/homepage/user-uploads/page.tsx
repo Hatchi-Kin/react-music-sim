d577c2193ec36ac1ac928003194aa6230400e1e0
Hatchi-Kin <130794142+Hatchi-Kin@users.noreply.github.com>
2024-07-14 19:24:03 +0200

    Favorites (#8)
    * base de favorites
    
    * first commit
    
    * changement de la position du titre de page
    
    * changement de la position du titre de page tracklist
    
    * la même
    
    * finish favorites
    
    * add navigation for future user-uploads
    
    ---------
    
    Co-authored-by: Riddlerjoke <jeremy.lardic@hotmail.fr>
    Co-authored-by: riddlerjoke <58588665+Riddlerjoke@users.noreply.github.com>

---
+++ app/(homepage)/homepage/user-uploads/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Header from "@/components/Header";
+// import ManageMyFavorites from "@/components/MyFavorites";
+
+export default function MyPlaylist() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    if (!token) {
+      router.push("/sign-in");
+    }
+  }, [router]);
+
+  return (
+    <div
+      className="
+          bg-neutral-900
+          h-full
+          w-full
+          overflow-hidden
+          overflow-y-auto
+          "
+    >
+      <Header title={'Manage your Uploads'}>Headers</Header>
+      {/* <ManageMyFavorites /> */}
+    </div>
+  );
+}
