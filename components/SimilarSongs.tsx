8e73646cdb68e64a70c8d8db3b256e4232ff4b80
Hatchi-Kin <j.needham@hotmail.fr>
2024-07-20 20:10:41 +0200

    forgot a token check in fetchSimilarSongs

--- components/SimilarSongs.tsx
+++ components/SimilarSongs.tsx
@@ -41,7 +41,11 @@ const SimilarSongs = () => {
     })
       .then((response) => {
         if (!response.ok) {
-          throw new Error(`Network response was not ok: ${response.statusText}`);
+          if (response.statusText === "Unauthorized") {
+            window.location.href = "/sign-in";
+          } else {
+            throw new Error(`Network response was not ok: ${response.statusText}`);
+          }
         }
         return response.json();
       })
